e17f7a521bd77723367b654ee7855791a26e90e8
GattlerGun <alexeyrub20@gmail.com>
2022-09-22 19:33:23 +1000

    Обновил итоговое дз

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,9 +2,9 @@ public class Calculator {
 	
 	public static void main(String[] args) {
 		System.out.println("Калькулятор\n");
-		int a = 0 + (int) (Math.random() * 100);
-		int b = 0 + (int) (Math.random() * 100);
-		char sign = '*';
+		int a = (int) (Math.random() * 10);
+		int b = (int) (Math.random() * 10);
+		char sign = '^';
 		int result = 0;
 		if((a >= 0) && (b >= 0)) {
 			if(sign == '+') {
@@ -16,8 +16,9 @@ public class Calculator {
 			} else if(sign == '/') {
 				result = a / b;
 			} else if(sign == '^') {
+				result = 1;
 				for(int i = 1; i < b; i++) {
-					result = a * a;
+					result *= a;
 				}
 			} else if(sign == '%') {
 				result = a % b;
@@ -25,7 +26,7 @@ public class Calculator {
 				System.out.println("Такой операции в калькуляторе не реализовано");
 			}
 			System.out.println(a + " " + sign + " " + b + " = " + result);
-		}else {
+		} else {
 			System.out.println("Калькулятор считает только положительные числа");
 		}
 	}
--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -4,22 +4,17 @@ public class MyFirstGame {
 		System.out.println("Игра - Угадай число\n");
 		int computerNum = 1 + (int) (Math.random() * 100);
 		int playerNum = 50;
-		boolean isTrue = true;
-		while(isTrue) {
-			if(playerNum == computerNum) {
-				System.out.println("Вы победили! Это число " + computerNum);
-				isTrue = false;
-			} else {
-				if(playerNum > computerNum) {
-					System.out.println("число " + playerNum + 
-							" больше того, что загадал компьютер");
-					playerNum--;
-				} else if(playerNum < computerNum){
-					System.out.println("число " + playerNum + 
-							" меньше того, что загадал компьютер");
-					playerNum++;
-				}
+		while(playerNum != computerNum) {
+			if(playerNum > computerNum) {
+				System.out.println("число " + playerNum + 
+						" больше того, что загадал компьютер");
+				playerNum--;
+			} else if(playerNum < computerNum){
+				System.out.println("число " + playerNum + 
+						" меньше того, что загадал компьютер");
+				playerNum++;
 			}
 		}
+		System.out.println("Вы победили! Это число " + computerNum);
 	}
 }
