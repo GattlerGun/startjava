b03be9776d54a311dc42b62fa4015d354ec3bc2e
GattlerGun <alexeyrub20@gmail.com>
2022-09-23 20:56:04 +1000

    Выполнил первое дз 2 урока

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+	boolean isMale = true;
+	String name = "Artem";
+	double height = 1.76;
+	double weight = 78;
+	int age = 23;
+
+	void move() {
+		System.out.println("moving");
+	}
+
+	void sit() {
+		System.out.println("sitting");
+	}
+
+	boolean Run() {
+		return true
+	}
+
+	String say() {
+		return "saying"
+	}
+
+	void learnJava() {
+		System.out.println("learning java");
+	}
+}
