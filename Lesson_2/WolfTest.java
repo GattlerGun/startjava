18eab1d74fa829089c5d6741567aff99e6a618c4
GattlerGun <alexeyrub20@gmail.com>
2022-09-24 18:33:00 +1000

    Выполнил 3 дз

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,20 +2,21 @@ public class WolfTest {
 	
 	public static void main(String[] args) {
 		Wolf wolf1 = new Wolf();
-		wolf1.sex = "Мужской";
-		wolf1.name = "Ричард";
-		wolf1.weight = 13.2;
-		wolf1.age = 10;
-		wolf1.color = "Серый";
-		System.out.println("Пол: " + wolf1.sex +
-				"\nКличка: " + wolf1.name +
-				"\nВес: " + wolf1.weight +
-				"\nВозраст: " + wolf1.age +
-				"\nЦвет: " + wolf1.color);
+		wolf1.setSex("Мужской");
+		wolf1.setName("Ричард");
+		wolf1.setWeight(13.2);
+		wolf1.setAge(10);
+		wolf1.setColor("Серый");
+		System.out.println("Пол: " + wolf1.getSex());
+		System.out.println("Кличка: " + wolf1.getName());
+		System.out.println("Вес: " + wolf1.getWeight());
+		System.out.println("Возраст: " + wolf1.getAge());
+		System.out.println("Цвет: " + wolf1.getColor());
 		wolf1.walk();
 		wolf1.sit();
 		wolf1.run();
 		wolf1.howl();
 		wolf1.hunt();
+		wolf1.getAge();
 	}
 }
