1fc33ace8b202c86e5d793538ae74ee2a6ba9eac
GattlerGun <alexeyrub20@gmail.com>
2022-09-25 23:17:36 +1000

    обновил калькулятор

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -2,68 +2,46 @@ public class Calculator {
 	
 	private int num1;
 	private int num2;
-	private int result;
 	private char sign;
 
-	public int getNum1() {
-		return num1;
+	public void setNum1(int num1) {
+			this.num1 = num1;
 	}
 
-	public void setNum1 (int num1) {
-		this.num1 = num1;
-	}
-
-	public int getNum2() {
-		return num2;
-	}
-
-	public void setNum2 (int num2) {
+	public void setNum2(int num2) {
 		this.num2 = num2;
 	}
 
-	public int getResult() {
-		return result;
-	}
-
-	public void setResult (int result) {
-		this.result = result;
-	}
-
-	public char getSign() {
-		return sign;
-	}
-
-	public void setSign (char sign) {
+	public void setSign(char sign) {
 		this.sign = sign;
 	}
 
-	public int calculation(char sign, int num1, int num2) {
-		switch(sign) {
-			case '+':
-				result = num1 + num2;
-				break;
-			case '-':
-				result = num1 - num2;
-				break;
-			case '*':
-				result = num1 * num2;
-				break;
-			case '/':
-				result = num1 / num2;
-				break;
-			case '^':
-				result = 1;
-				for(int i = 0; i < num2; i++) {
-					result *= num1;
+	public int calculate(char sign, int num1, int num2) {
+		if((num1 < 0) || (num2 < 0)) {
+			System.out.println("только положительные числа!!!");
+		} else {
+			switch(sign) {
+				case '+':
+					return num1 + num2;
+				case '-':
+					return num1 - num2;
+				case '*':
+					return num1 * num2;
+				case '/':
+					return num1 / num2;
+				case '^':
+					int result = 1;
+					for(int i = 0; i < num2; i++) {
+						result *= num1;
 					}
-				break;
-			case '%':
-				result = num1 % num2;
-				break;
-			default:
-				 System.out.println("Такой операции в калькуляторе не реализовано");
-				 break;
+					return result;
+				case '%':
+					return num1 % num2;
+				default:
+					 System.out.println("Такой операции в калькуляторе не реализовано");
+					 break;
+			}
 		}
-		return result;
+		return sign;
 	}
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,31 +4,23 @@ public class CalculatorTest {
 
 	public static void main(String[] args) {
 		String userAnswer = "yes";
-		boolean isYes = true;
 		Scanner scan = new Scanner(System.in);
 		Calculator calc = new Calculator();
-		while(isYes) {
-			if(userAnswer.equals("yes")){
-				System.out.println(userAnswer);
+		while(!userAnswer.equals("no")) {
+			if(userAnswer.equals("yes")) {
 				System.out.println("Введите первое число: ");
-				calc.setNum1(scan.nextInt());
-				int num1 = calc.getNum1();
+				int num1 = scan.nextInt();
+				calc.setNum1(num1);
 				System.out.println("Введите второе число: ");
-				calc.setNum2(scan.nextInt());
-				int num2 = calc.getNum2();
+				int num2 = scan.nextInt();
+				calc.setNum2(num2);
 				System.out.println("Введите математическую операцию(+, -, /, *, ^, %):");
-				calc.setSign(scan.next().charAt(0));
-				char sign = calc.getSign();
-				if((num1 >= 0) && (num2 >= 0)) {
-					int result = calc.calculation(sign, num1, num2);
-					System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
-				} else {
-					System.out.println("только положительные числа!");
-				}
+				char sign = scan.next().charAt(0);
+				calc.setSign(sign);
+				int result = calc.calculate(sign, num1, num2);
+				System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
 				System.out.println("Хотите продолжить вычисления? [yes/no]:");
 				userAnswer = scan.next();
-			} else if(userAnswer.equals("no")) {
-				isYes = false;
 			} else {
 				System.out.println("Хотите продолжить вычисления? [yes/no]:");
 				userAnswer = scan.next();
